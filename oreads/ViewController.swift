f95c44c940ef465d5d240f7de71eb1782ee59bdc
Sergey Filatov <s.filatov@corp.mail.ru>
2019-01-26 18:47:54 +0300

    Initial commit

--- oreads/ViewController.swift
+++ oreads/ViewController.swift
@@ -6,15 +6,39 @@
 //  Copyright © 2019 Sergey Filatov. All rights reserved.
 //
 
+import AVKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var play: UIButton!
+    @IBOutlet weak var Pause: UIButton!
+    
+    var player: AVPlayer?
+    var audioUrl: String?
+    var isAudioPlaying: Bool?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        play.setImage(UIImage(named:"play.png"),for:.normal)
+        isAudioPlaying = false
+        audioUrl = "https://hb.bizmrg.com/oreads/Mythos%20(Unabridged).m4b"
+        player = AVPlayer(url: URL.init(string: audioUrl!)!)
     }
-
+    
+    @IBAction func play(_ playBtn: UIButton) {
+        if !isAudioPlaying! {
+            player!.play()
+            isAudioPlaying = true
+            playBtn.setImage(UIImage(named:"pause.png"),for:.normal)
+            playBtn.setImage(UIImage(named:"pause.png"),for:.highlighted)
+        } else {
+            player!.pause()
+            isAudioPlaying = false
+            playBtn.setImage(UIImage(named:"play.png"),for:.normal)
+            playBtn.setImage(UIImage(named:"pause.png"),for:.highlighted)
+        }
+    }
 
 }
 
